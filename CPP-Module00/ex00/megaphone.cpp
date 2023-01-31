2f11c77689f7d24228fb4b5b23939ff1df6dea7f
nicolike <nicolike3.0@gmail.com>
2023-01-31 20:52:44 +0100

    started ex00 from cpp00

---
+++ CPP-Module00/ex00/megaphone.cpp
@@ -0,0 +1,30 @@
+#include <cctype>
+#include <iostream>
+#include <string>
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	size_t	j;
+
+	i = 0;
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
+	else
+	{
+		while (++i < argc)
+		{
+			j = -1;
+			std::string string = std::string(argv[i]);
+			while (++j < string.length())
+			{
+				std::toupper(string[j]);
+			}
+			std::cout << string << std::endl;
+		}
+	}
+
+	return (0);
+}
