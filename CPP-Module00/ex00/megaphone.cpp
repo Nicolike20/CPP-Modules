01b273388e35f28ee9d782d002f162faf64545bc
nicolike <nicolike3.0@gmail.com>
2023-02-01 13:57:10 +0100

    started cpp00 ex01

--- CPP-Module00/ex00/megaphone.cpp
+++ CPP-Module00/ex00/megaphone.cpp
@@ -1,30 +1,19 @@
-#include <cctype>
 #include <iostream>
+#include <cstdio>
 #include <string>
 
 int	main(int argc, char **argv)
 {
-	int		i;
-	size_t	j;
-
-	i = 0;
-	if (argc == 1)
-	{
+	if (argc == 1) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
 	}
-	else
-	{
-		while (++i < argc)
-		{
-			j = -1;
-			std::string string = std::string(argv[i]);
-			while (++j < string.length())
-			{
-				std::cout << (char)std::toupper(string[j]);
-			}
+	for (int i = 1; i < argc; ++i) {
+		std::string string = std::string(argv[i]);
+		for (size_t j = 0; j < string.length(); ++j) {
+			std::cout << (char)std::toupper(string[j]);
 		}
-		std::cout << std::endl;
 	}
-
+	std::cout << std::endl;
 	return (0);
 }
---
+++ CPP-Module00/ex01/phonebook.cpp
@@ -0,0 +1,8 @@
+#include <string>
+#include <iostream>
+int main(void){
+	std::string name;
+	std::cin >> name;
+	std::cout << "you entered: " << name << std::endl;
+return (0);
+}
