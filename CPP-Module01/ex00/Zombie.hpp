dd48b318939dae071b072209a5706080e0d22d41
Nicolas maria Ortolan soto <nortolan@c3r11s2.42madrid.com>
2023-03-15 14:43:14 +0100

    started working on cpp01

---
+++ CPP-Module01/ex00/Zombie.hpp
@@ -0,0 +1,14 @@
+#ifndef ZOMBIE_HPP_
+# define ZOMBIE_HPP_
+
+# include <string>
+
+class Zombie {
+    public:
+        void announce(void); //esto privado o publico?
+        //constructor y destructor?
+    private:
+        string _name;
+};
+
+#endif
