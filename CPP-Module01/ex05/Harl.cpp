13bc6ef2e14ef7dba78217c0452c657f58f9e66a
nicolike <nicolike3.0@gmail.com>
2023-04-14 18:00:49 +0200

    cpp01/ex05 done i think

--- CPP-Module01/ex05/Harl.cpp
+++ CPP-Module01/ex05/Harl.cpp
@@ -1,1 +1,41 @@
 #include "Harl.hpp"
+
+Harl::Harl() {}
+
+Harl::~Harl() {}
+
+void Harl::debug(void) {
+	std::cout << "[ DEBUG ]\n"
+		<< "I love having extra bacon for my"
+		<< "7XL-double-cheese-triple-picklespecial-ketchup burger. I really do!\n";
+}
+
+void Harl::info(void) {
+	std::cout << "[ INFO ]\n"
+		<< "I cannot believe adding extra bacon costs more money. You didn’t put"
+		<< "enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
+}
+
+void Harl::warning(void) {
+	std::cout << "[ WARNING ]\n"
+		<< "I think I deserve to have some extra bacon for free. I’ve been coming"
+		<< "for years whereas you started working here since last month.\n";
+}
+
+void Harl::error(void) {
+	std::cout << "[ ERROR ]\n"
+		<< "This is unacceptable! I want to speak to the manager now.\n";
+}
+
+void Harl::complain(std::string level) {
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	func functions[4] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+
+	for (int i = 0; i < 4; i++) {
+		if (!levels[i].compare(level)) {
+			(this->*(functions[i]))();
+			return ;
+		}
+	}
+	std::cout << "[ INVALID COMMAND ]\n";
+}
--- CPP-Module01/ex05/main.cpp
+++ CPP-Module01/ex05/main.cpp
@@ -1,5 +1,11 @@
 #include "Harl.hpp"
 
-int	main(){
+int	main(int argc, char **argv){
+	if (argc != 2) {
+		return (1);
+	}
+	Harl h;
+	h.complain(argv[1]);
 
+	return (0);
 }
