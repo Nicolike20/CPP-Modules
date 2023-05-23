db0d1e422479c8826638689e775e170cb84f7697
Nicolas maria Ortolan soto <nicolike3.0@gmail.com>
2023-05-23 16:43:42 +0200

    removed some comments

--- CPP-Module02/ex02/Fixed.hpp
+++ CPP-Module02/ex02/Fixed.hpp
@@ -14,24 +14,24 @@ class Fixed {
 
 		Fixed& operator=(const Fixed &rhs);
 
-		bool operator<(const Fixed &rhs) const; //es necesario que sean const?
+		bool operator<(const Fixed &rhs) const;
 		bool operator>(const Fixed &rhs) const;
 		bool operator<=(const Fixed &rhs) const;
 		bool operator>=(const Fixed &rhs) const;
 		bool operator==(const Fixed &rhs) const;
 		bool operator!=(const Fixed &rhs) const;
 
-		Fixed operator+(const Fixed &rhs) const; //es necesario que sean const?
+		Fixed operator+(const Fixed &rhs) const;
 		Fixed operator-(const Fixed &rhs) const;
 		Fixed operator*(const Fixed &rhs) const;
 		Fixed operator/(const Fixed &rhs) const;
 
-		Fixed& operator++(); //estas no tienen que ser const? //pre
-		Fixed operator++(int);                                //post
+		Fixed& operator++();	//pre
+		Fixed operator++(int);	//post
 		Fixed& operator--();
 		Fixed operator--(int);
 
-		static Fixed& min(Fixed &lhs, Fixed &rhs); //estas no tienen que ser const?
+		static Fixed& min(Fixed &lhs, Fixed &rhs);
 		static const Fixed& min(const Fixed &lhs, const Fixed &rhs);
 		static Fixed& max(Fixed &lhs, Fixed &rhs);
 		static const Fixed& max(const Fixed &lhs, const Fixed &rhs);
