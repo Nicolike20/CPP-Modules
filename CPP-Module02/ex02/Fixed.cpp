c3b5366903daa27374d1390063c909b29463220d
Nicolas maria Ortolan soto <nicolike3.0@gmail.com>
2023-05-23 14:01:41 +0200

    cpp02/ex02 working, need to check stuff and remove comments

--- CPP-Module02/ex02/Fixed.cpp
+++ CPP-Module02/ex02/Fixed.cpp
@@ -1,31 +1,31 @@
 #include "Fixed.hpp"
 
 Fixed::Fixed() {
-    std::cout << "Default constructor called\n";
+//    std::cout << "Default constructor called\n";
     _raw = 0;
 }
 
 Fixed::Fixed(const Fixed &copy) {
-    std::cout << "Copy constructor called\n";
+//    std::cout << "Copy constructor called\n";
     *this = copy;
 }
 
 Fixed::Fixed(int i) {
-    std::cout << "Int constructor called\n";
+//    std::cout << "Int constructor called\n";
     _raw = i << frac; //esto es como multiplicar por 2^frac
 }
 
 Fixed::Fixed(float f) {
-    std::cout << "Float constructor called\n";
+//    std::cout << "Float constructor called\n";
     _raw = roundf(f * (1 << frac)); //esto es como multiplicar por 2^frac pero hay que hacerlo asi para que no se queje por conversiones
 }
 
 Fixed::~Fixed() {
-    std::cout << "Destructor called\n";
+//    std::cout << "Destructor called\n";
 }
 
 Fixed& Fixed::operator=(const Fixed &rhs) {
-    std::cout << "Assignation operator called\n";
+//    std::cout << "Assignation operator called\n";
     if (this != &rhs) {
         _raw = rhs._raw;
     }
@@ -97,13 +97,13 @@ Fixed Fixed::operator-(const Fixed &rhs) const {
 
 Fixed Fixed::operator*(const Fixed &rhs) const {
     Fixed fixed;
-    fixed._raw = _raw * rhs._raw; //to float?
+    fixed = this->toFloat() * rhs.toFloat(); //to float?
     return fixed;
 }
 
 Fixed Fixed::operator/(const Fixed &rhs) const {
     Fixed fixed;
-    fixed._raw = _raw / rhs._raw; //to float?
+    fixed = this->toFloat() / rhs.toFloat(); //to float?
     return fixed;
 }
 
@@ -116,9 +116,8 @@ Fixed& Fixed::operator++() {
 
 Fixed Fixed::operator++(int) {
     Fixed fixed;
-    std::cout << "hola?\n";
     fixed = *this;
-    ++*this;
+    ++_raw;
     return fixed;
 }
 
@@ -130,7 +129,7 @@ Fixed& Fixed::operator--() {
 Fixed Fixed::operator--(int) {
     Fixed fixed;
     fixed = *this;
-    --*this;
+    --_raw;
     return fixed;
 }
 
@@ -163,11 +162,11 @@ float Fixed::toFloat(void) const {
 }
 
 int Fixed::getRawBits(void) const {
-    std::cout << "getRawBits member function called\n";
+//    std::cout << "getRawBits member function called\n";
     return (_raw);
 }
 
 void Fixed::setRawBits(const int raw) {
-    std::cout << "setRawBits member function called\n";
+//    std::cout << "setRawBits member function called\n";
     _raw = raw;
 }
