5f8fc7d01a5fd3f2304a173faa7761075093e294
Nicolas maria Ortolan soto <nicolike3.0@gmail.com>
2023-05-25 18:33:09 +0200

    removed some comments

--- CPP-Module02/ex03/bsp.cpp
+++ CPP-Module02/ex03/bsp.cpp
@@ -1,20 +1,16 @@
 #include "Point.hpp"
 
 float areaCalc(Point const p1, Point const p2, Point const p3) {
-    return fabs((p1.getX().toFloat() * (p2.getY().toFloat() - p3.getY().toFloat()) + p2.getX().toFloat() * (p3.getY().toFloat() - p1.getY().toFloat()) + p3.getX().toFloat() *(p1.getY().toFloat() - p2.getY().toFloat())) / 2.0f);
+    return fabs((p1.getX().toFloat() * (p2.getY().toFloat() - p3.getY().toFloat()) +
+    p2.getX().toFloat() * (p3.getY().toFloat() - p1.getY().toFloat()) +
+    p3.getX().toFloat() *(p1.getY().toFloat() - p2.getY().toFloat())) / 2.0f);
 }
 
 bool bsp(Point const a, Point const b, Point const c, Point const point) {
-//    std::cout << "hola?\n" << a.getX() << " " << b.getX() << " " << c.getX() << " " << point.getX() << "\n";
-
     float area = areaCalc(a, b, c);
     float ap1 = areaCalc(point, a, b);
     float ap2 = areaCalc(point, a, c);
     float ap3 = areaCalc(point, b, c);
-
-
-//    std::cout << "AREA:  " << area << "\n";
-//    std::cout << "TOTAL: " << ap1 + ap2 + ap3 << "\n";
     
     return ((area == ap1 + ap2 + ap3) && ap1 && ap2 && ap3);
 }
--- CPP-Module02/ex03/main.cpp
+++ CPP-Module02/ex03/main.cpp
@@ -3,52 +3,49 @@
 bool bsp(Point const a, Point const b, Point const c, Point const point);
 
 int main( void ) {
+
+	/*Triangle Points*/
     Point const a(0, 0);
     Point const b(10, 30);
     Point const c(20, 0);
+
+	/*Testing Points*/
     Point const point(10, 15);
     Point const outside(30, 15);
     Point const limitTest(10, 1);
     Point const vertex(10, 30);
     Point const side(10, 0);
 
-//    std::cout << "hola?\n" << a.getX() << " " << b.getX() << " " << c.getX() << " " << point.getX() << "\n";
-//    bool res1 = bsp(a, b, c, point);        //dentro
-//    bool res2 = bsp(a, b, c, otro);         //fuera
-//    bool res3 = bsp(a, b, c,limitDentro);   //dentro
-//    bool res4 = bsp(a, b, c, vertice);      //fuera
-//    bool res5 = bsp(a, b, c, lado);         //fuera
-
 	std::cout << "Point: (" << point.getX() << ", " << point.getY() << ")\n";
-    if (bsp(a, b, c, point)) {			// dentro
+    if (bsp(a, b, c, point)) {			// Inside
         std::cout << "Inside\n\n";
     } else {
         std::cout << "Outside\n\n";
     }
 
 	std::cout << "Point: (" << outside.getX() << ", " << outside.getY() << ")\n";
-    if (bsp(a, b, c, outside)) {		// fuera
+    if (bsp(a, b, c, outside)) {		// Outside
         std::cout << "Inside\n\n";
     } else {
         std::cout << "Outside\n\n";
     }
 
 	std::cout << "Point: (" << limitTest.getX() << ", " << limitTest.getY() << ")\n";
-    if (bsp(a, b, c,limitTest)) {		// dentro
+    if (bsp(a, b, c,limitTest)) {		// Inside
         std::cout << "Inside\n\n";
     } else {
         std::cout << "Outside\n\n";
     }
 
 	std::cout << "Point: (" << vertex.getX() << ", " << vertex.getY() << ")\n";
-    if (bsp(a, b, c, vertex)) {			// fuera
+    if (bsp(a, b, c, vertex)) {			// Outside
         std::cout << "Inside\n\n";
     } else {
         std::cout << "Outside\n\n";
     }
 
 	std::cout << "Point: (" << side.getX() << ", " << side.getY() << ")\n";
-    if (bsp(a, b, c, side)) {			// fuera
+    if (bsp(a, b, c, side)) {			// Outside
         std::cout << "Inside\n\n";
     } else {
         std::cout << "Outside\n\n";
@@ -57,6 +54,5 @@ int main( void ) {
     return 0;
 
 //TODO: crear multiples tests en main; ->  testear esto a jierro;
-//      cambiar nombre de las variables que estan en español
 //      comprobar todos los detalles del modulo; quitar comentarios innecesarios;
 }
