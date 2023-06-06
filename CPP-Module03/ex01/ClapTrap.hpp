3a04312b562f0b2d98fee63206a8fa4b499ff9f8
Nicolas maria Ortolan soto <nicolike3.0@gmail.com>
2023-06-06 18:42:19 +0200

    working on cpp03/ex01

---
+++ CPP-Module03/ex01/ClapTrap.hpp
@@ -0,0 +1,26 @@
+#ifndef CLAPTRAP_HPP_
+# define CLAPTRAP_HPP_
+
+# include <string>
+# include <iostream>
+
+class ClapTrap {
+	public:
+		ClapTrap();
+		ClapTrap(std::string name);
+		ClapTrap(const ClapTrap &copy);
+		~ClapTrap();
+
+		ClapTrap& operator=(const ClapTrap &other);
+
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+	private:
+		std::string _name;
+		int _hp;
+		int _ep;
+		int _ad;
+};
+
+#endif
