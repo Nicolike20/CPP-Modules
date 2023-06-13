ae9ad0114fe913425387846ed27c324516c46b26
nicolike <nicolike3.0@gmail.com>
2023-06-13 20:01:54 +0200

    added more tests in cpp04/ex00

--- CPP-Module04/ex00/main.cpp
+++ CPP-Module04/ex00/main.cpp
@@ -1,21 +1,38 @@
 #include "Dog.hpp"
 #include "Cat.hpp"
+#include "WrongCat.hpp"
 
 int main()
 {
-	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
+	const Animal* animal = new Animal();
+	const Animal* dog = new Dog();
+	const Animal* cat = new Cat();
+	const WrongAnimal* wrongAnimal = new WrongAnimal();
+	const WrongAnimal* wrongCat = new WrongCat();
 
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); //will output the cat sound!
-	j->makeSound();
-	meta->makeSound();
-	//...TODO: more tests!!!!!
+	std::cout << "\n";
+
+	std::cout << animal->getType() << "\n";
+	std::cout << dog->getType() << "\n";
+	std::cout << cat->getType() << "\n";
+	animal->makeSound();
+	dog->makeSound();
+	cat->makeSound();
+
+	std::cout << "\n";
+
+	std::cout << wrongAnimal->getType() << "\n";
+	std::cout << wrongCat->getType() << "\n";
+	wrongAnimal->makeSound();
+	wrongCat->makeSound();
+
+	std::cout << "\n";
 	
-	delete meta;
-	delete j;
-	delete i;
+	delete animal;
+	delete dog;
+	delete cat;
+	delete wrongAnimal;
+	delete wrongCat;
+
 return 0;
 }
