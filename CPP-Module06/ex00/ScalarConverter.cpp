b5907cbd217c90d88935722bb31a3b9eabd4678a
Nicolas maria Ortolan soto <nicolike3.0@gmail.com>
2023-12-13 15:45:18 +0100

    yeah

--- CPP-Module06/ex00/ScalarConverter.cpp
+++ CPP-Module06/ex00/ScalarConverter.cpp
@@ -1,1 +1,15 @@
 #include "ScalarConverter.hpp"
+		
+ScalarConverter() {
+    std::cout << "ScalarConverter default constructor called\n";
+}
+ScalarConverter(const ScalarConverter &copy) {
+    //*this = copy;
+    std::cout << "ScalarConverter copy constructor called\n";
+}
+
+~ScalarConverter() {
+    std::cout << "ScalarConverter copy destructor called\n";
+}
+
+ScalarConverter& operator=(const ScalarConverter &other); //cozas
