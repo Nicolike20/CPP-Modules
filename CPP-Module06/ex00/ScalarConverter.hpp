38a4673fce7c7e00ad9a26bd9ce1c3bc03c517fa
Nicolas maria Ortolan soto <nicolike3.0@gmail.com>
2023-12-13 15:40:37 +0100

    added constructors to ScalarConverter

--- CPP-Module06/ex00/ScalarConverter.hpp
+++ CPP-Module06/ex00/ScalarConverter.hpp
@@ -3,5 +3,12 @@
 
 class ScalarConverter {
 	public:
+		ScalarConverter(); //constructores en privado?
+		ScalarConverter(const ScalarConverter &copy);
+		~ScalarConverter(); //y destructor?
+
+		ScalarConverter& operator=(const ScalarConverter &other);
+
+		void convert(/*que coño va aqui*/);
 	private:
 };
