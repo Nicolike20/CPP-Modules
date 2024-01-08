781bf5137362fb9ba7344d852653ca8253d2499e
Nicolas maria Ortolan soto <nicolike3.0@gmail.com>
2024-01-08 13:50:42 +0100

    added constructors and destructor for Serializer class

--- CPP-Module06/ex01/Serializer.cpp
+++ CPP-Module06/ex01/Serializer.cpp
@@ -1,1 +1,22 @@
 #include "Serializer.hpp"
+
+Serializer::Serializer() {
+	std::cout << "Seralizer default constructor called\n";
+}
+
+Serializer::Serializer(const Serializer &copy) {
+	*this = copy;
+	std::cout << "Serializer copy constructor called\n";
+}
+
+Serializer::~Serializer(){
+	std::cout << "Serializer destructor called\n";
+}
+
+Serializer& Serializer::operator=(const Serializer &other){
+	(void) other;
+	return *this;
+}
+
+        static uintptr_t serialize(Data* ptr); //revisar static
+        static Data* deserialize(uintptr_t raw);
