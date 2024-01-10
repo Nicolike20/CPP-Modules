83a24ef1070f3100d9d39954e64c2f21959110d0
Nicolas maria Ortolan soto <nicolike3.0@gmail.com>
2024-01-10 20:40:22 +0100

    added functions from the subject

--- CPP-Module06/ex02/main.cpp
+++ CPP-Module06/ex02/main.cpp
@@ -2,6 +2,18 @@
 
 //POR QUE DYNAMIC_CAST?
 
+//https://stackoverflow.com/questions/351845/finding-the-type-of-an-object-in-c
+
+Base * generate(void) {
+    //do stuff here
+}
+void identify(Base* p) {
+    //do stuff here
+}
+void identify(Base& p) {
+    //do stuff here
+}
+
 int main() {
     return (0);
 }
