884495756fd7c5c5ba3bc553e76a7058af4443e3
Nicolas maria Ortolan soto <nicolike3.0@gmail.com>
2024-01-11 17:59:50 +0100

    cpp07/ex00 working, need to check parameters

--- CPP-Module07/ex00/main.cpp
+++ CPP-Module07/ex00/main.cpp
@@ -8,11 +8,15 @@ int main(void) {
 
     ::swap(a, b); // asi o ::swap<int>(a, b)?
     std::cout << "a: " << a << ", b: " << b << "\n";
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 
     std::string s1 = "string1";
     std::string s2 = "string2";
 
     ::swap(s1, s2); // asi o ::swap<std::string>(s1, s2)?
-    std::cout << "s1: " << s1 << ", s2: " << s2 << "\n";
+    std::cout << "\ns1: " << s1 << ", s2: " << s2 << "\n";
+    std::cout << "min(s1, s2) = " << ::min(s1, s2) << std::endl;
+    std::cout << "max(s1, s2) = " << ::max(s1, s2) << std::endl;
     return (0);
 }
--- CPP-Module07/ex00/whatever.hpp
+++ CPP-Module07/ex00/whatever.hpp
@@ -7,6 +7,10 @@ template <class T> void swap(T &x, T &y) {
     y = temp;
 }
 
-template <class T> T min(T x, T y) {} //check esto
+template <class T> T min(T x, T y) {
+    return x >= y ? y : x; //como checkeo que x e y son del mismo tipo? y que se pueden comparar?
+}
 
-template <class T> T max(T x, T y) {} //check esto
+template <class T> T max(T x, T y) {
+    return x <= y ? y : x; //como checkeo que x e y son del mismo tipo? y que se pueden comparar?
+}
