0d3e6959402b0622321ff930b03570ce88abb032
Nicolas maria Ortolan soto <nicolike3.0@gmail.com>
2024-01-15 16:27:44 +0100

    template iter done, needs testing

--- CPP-Module07/ex01/iter.hpp
+++ CPP-Module07/ex01/iter.hpp
@@ -1,4 +1,12 @@
 #ifndef ITER_HPP_
 # define ITER_HPP_
 
+template <class T> void iter(T *arr, T len, void (*f) (T /*const T&?q*/)) { //int len? size_t len?
+    for (int i = 0; i < len; i++) {
+        f(arr[i]);
+    }
+};
+
 #endif
+
+/*"There is no semantic difference between class and typename in a type-parameter-key."*/
