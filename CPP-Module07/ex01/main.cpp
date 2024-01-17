654304e247fc04ae27ed2b01485998a19f64546c
Nicolas maria Ortolan soto <nicolike3.0@gmail.com>
2024-01-17 16:15:33 +0100

    cpp07/ex01 done. maybe needs more tests in main

--- CPP-Module07/ex01/main.cpp
+++ CPP-Module07/ex01/main.cpp
@@ -6,15 +6,24 @@ template <class T> void printIter(const T& i) {
     std::cout << i << "\n";
 }
 
+template <class T> void doublePrint(const T& i) {
+    std::cout << i + i << "\n";
+}
+
 int main(void) {
     int numbers[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     std::string words[10] = {"test", "hello", "world", "42", "string", "aaa", "bbb", "ccc", "ddd", "eee"};
 
     std::cout << "Number array test:\n";
     ::iter(numbers, 10, &printIter);
+    std::cout << "\n";
+    ::iter(numbers, 10, &doublePrint);
+
     std::cout << "\nString array test:\n";
     ::iter(words, 10, &printIter);
+    std::cout << "\n";
+    ::iter(words, 10, &doublePrint);
 
-    //add another test (another function like printIter);
+    //las funciones de testeo son suficientes?
     return (0);
 }
