ad1e3fd7c1da57336fac1005819d45d0d619b78a
Nicolas maria Ortolan soto <nicolike3.0@gmail.com>
2024-01-17 17:16:21 +0100

    creating class template

--- CPP-Module07/ex02/Array.hpp
+++ CPP-Module07/ex02/Array.hpp
@@ -1,4 +1,18 @@
 #ifndef ARRAY_HPP_
 # define ARRAY_HPP_
 
+//optional file Array.tpp?
+
+template <class T> class Array {
+    public:
+        Array();
+        Array(unsigned int n);
+        Array(const Array& copy);
+        ~Array();
+
+        Array& operator=(const Array& other);
+        int size() const; //unsigned int?
+    private:
+};
+
 #endif
