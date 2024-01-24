8fc310494db3940f953537dfd969232ebed7f4ea
Nicolas maria Ortolan soto <nicolike3.0@gmail.com>
2024-01-24 13:12:11 +0100

    constructors and destructors done, trying to compile

--- CPP-Module07/ex02/Array.hpp
+++ CPP-Module07/ex02/Array.hpp
@@ -1,6 +1,7 @@
 #ifndef ARRAY_HPP_
 # define ARRAY_HPP_
 
+# include <exception>
 
 template <class T> class Array {
     public:
@@ -10,21 +11,35 @@ template <class T> class Array {
         Array(unsigned int n) : array_(new T[n]), size_(n) {
             //std::cout?;
         }
-        Array(const Array& copy) {
-            //do stuff here;
+        Array(const Array& copy) /*Array()?*/ /*: array_(NULL)?*/ {
+            this* = copy;
+            //std::cout?;
         }
         ~Array() {
-            delete[] this->array_;
+            delete[] array_; //this->array_?
         }
 
         Array& operator=(const Array& other) {
-            //do stuff here;
+            if (this != &other) {
+                if (this->array_ != NULL) {
+                    delete[] this->array_; //quitar los this->?;
+                }
+                this->array_ = new T[other.size_];
+                this->size_ = other.size_;
+                for (int i = 0; i < this->size_; i++) {
+                    this->array_[i] = other.array_[i];
+                }
+                return *this;
+            }
+            return *this;
         }
-        int size() const {
-            //do stuff here;
+        unsigned int size() const {
+            return size_; //this->size_?
         } //unsigned int? size_t?
 
-        //add exception for out of index call
+        class InvalidIndexException : public std::exception {
+            public: const char* what() const throw(); //do stuff here?;
+        };
     private:
         T *array_;
         unsigned int size_;
