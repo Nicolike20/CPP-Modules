86b8af203a20170ab4967ff89a26df9fe0bf7e06
Nicolas maria Ortolan soto <nicolike3.0@gmail.com>
2024-01-24 12:51:43 +0100

    need to do copy constructor and ossignment operator

--- CPP-Module07/ex02/Array.hpp
+++ CPP-Module07/ex02/Array.hpp
@@ -1,25 +1,20 @@
 #ifndef ARRAY_HPP_
 # define ARRAY_HPP_
 
-//optional file Array.tpp?
-//  |
-//  v
-//It's typically used to denote that the source file contains implementations of 
-//templated methods (as opposed to putting the implementation in the header file).
 
 template <class T> class Array {
     public:
-        Array() {
-            //do stuff here;
+        Array() : array_(NULL), size_(0) {
+            //std::cout?;
         }
-        Array(unsigned int n) {
-            //do stuff here;
+        Array(unsigned int n) : array_(new T[n]), size_(n) {
+            //std::cout?;
         }
         Array(const Array& copy) {
             //do stuff here;
         }
         ~Array() {
-            //do stuff here;
+            delete[] this->array_;
         }
 
         Array& operator=(const Array& other) {
@@ -31,11 +26,11 @@ template <class T> class Array {
 
         //add exception for out of index call
     private:
-        //private attributes?
+        T *array_;
+        unsigned int size_;
 };
 
 //figure out how to work with class templates, define what the constructors and destructor do
-//(either in this file or in a .tpp file) -> figure out if i should use .tpp files (it doesn't seem to matter that much)
 //add header to work with exceptions
 //why the need to use new[]
 //Try to compile int * a = new int(); then display *a
