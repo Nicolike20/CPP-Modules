2241668931447855b623257b9fdc2ab67357c187
Nicolas maria Ortolan soto <nicolike3.0@gmail.com>
2024-02-01 16:04:07 +0100

    started working on cpp08

---
+++ CPP-Module08/ex00/easyfind.hpp
@@ -0,0 +1,15 @@
+#ifndef EASYFIND_HPP_
+# define EASYFIND_HPP_
+
+# include <algorithm>
+
+/*template <class T> class easyfind {
+    public:
+    private:
+};*/
+
+template <class T> void easyfind(const T& t, int n) { //returns void?
+
+}
+
+#endif
