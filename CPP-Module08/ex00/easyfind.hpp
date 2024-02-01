0e90412747eeef2434535d81dc6fcd60688034db
Nicolas maria Ortolan soto <nicolike3.0@gmail.com>
2024-02-01 16:40:04 +0100

    cpp08/ex01 working, need to add better tests and change the return values

--- CPP-Module08/ex00/easyfind.hpp
+++ CPP-Module08/ex00/easyfind.hpp
@@ -1,6 +1,8 @@
 #ifndef EASYFIND_HPP_
 # define EASYFIND_HPP_
 
+# include <iostream>
+# include <vector>
 # include <algorithm>
 
 /*template <class T> class easyfind {
@@ -9,7 +11,7 @@
 };*/
 
 template <class T> void easyfind(const T& t, int n) { //returns void?
-
+    std::find(t.begin(), t.end(), n) != t.end() ? std::cout << "Element found\n" : std::cout << "Element not found\n";
 }
 
 #endif
--- CPP-Module08/ex00/main.cpp
+++ CPP-Module08/ex00/main.cpp
@@ -1,6 +1,23 @@
+#include "easyfind.hpp"
 
 int main(void) {
 
+    std::vector<int> v1;
+
+    for (int i = 0; i <= 5; i++) {
+        v1.push_back(i);
+    }
+
+    std::cout << "Vector v1:";
+    for (std::vector<int>::const_iterator i = v1.begin(); i != v1.end(); i++) {
+        std::cout << " " << *i;
+    }
+    std::cout << "\n";
+
+    easyfind(v1, 3);
+    easyfind(v1, 6);
+    easyfind(v1, 5);
+    //pulir los tests
     return (0);
 }
 
