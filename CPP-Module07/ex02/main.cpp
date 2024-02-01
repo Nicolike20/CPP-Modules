e0d89edede35764ccc9e8bdad1aa98794fa4bc42
Nicolas maria Ortolan soto <nicolike3.0@gmail.com>
2024-02-01 15:21:46 +0100

    added a comment

--- CPP-Module07/ex02/main.cpp
+++ CPP-Module07/ex02/main.cpp
@@ -100,6 +100,10 @@ int main(void) {
     //TODO ex00: comprobar en el subject si es importante lo de que sean la misma clase
     //TODO ex01: comprobar qué funciones suele hacer la gente para los test
     //TODO ex02: plantear mas tests y check subject
+    /*en la eval hay tests que tienes que pasar con tu codigo y que funke,
+    tengo lo que creo que es la version antigua del ex01.cpp y funciona con mi código,
+    los otros dos no tengo ni idea de como son, renta investigar un poco antes de corregirlo
+    y prepararse por si hay que ponerse a defender como un metapod*/
     return (0);
 }
 
