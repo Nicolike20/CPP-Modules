637a24ce18505a2e8d28dc10a15cc90c3d394c52
Nicolas maria Ortolan soto <nicolike3.0@gmail.com>
2024-02-22 15:17:20 +0100

    added a typedef for the iterator in stack, and a lot of useful info in comments

--- CPP-Module08/ex02/MutantStack.hpp
+++ CPP-Module08/ex02/MutantStack.hpp
@@ -18,12 +18,16 @@ template <class T, class Container = std::deque<T> > class MutantStack
             }
             return *this;
         }
+
+        typedef typename std::stack<T, Container>::container_type::iterator iterator;
     private:
 };
 
-//investigar stack
-//investigar como añadir iteradores
-// "         si estoy montando el template bien
-//wtf
+//necesito implementar los otros iteradores de deque? (const_iterator, const_reverse_iterator, reverse_iterator)
 
+//lo de container = std::deque<T> es para poder implementar los iterators?
+//typedef typename -> https://stackoverflow.com/questions/18385418/c-meaning-of-a-statement-combining-typedef-and-typename
+//::container_type accede al tipo de container que usa stack (esto se puede explicar mejor) en este caso deque<T>
+//deque tiene dentro un typedef iterator, el cual estamos accediendo para crear nuestro iterador. "typedef typename _Base::iterator                   iterator;"
+//https://learn.microsoft.com/es-es/cpp/standard-library/deque-class?view=msvc-170
 #endif
