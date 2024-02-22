f51d7f173da8d290e3c9c959472dad6634c9a92d
Nicolas maria Ortolan soto <nicolike3.0@gmail.com>
2024-02-22 14:06:23 +0100

    added canonical stuff to MutantStack

--- CPP-Module08/ex02/MutantStack.hpp
+++ CPP-Module08/ex02/MutantStack.hpp
@@ -3,10 +3,23 @@
 
 # include <stack>
 
-template <class T> class MutantStack : public std::stack<T, Container> {
+template <class T, class Container = std::deque<T> > class MutantStack
+: public std::stack<T, Container> {
     public:
+        MutantStack() {}
+        MutantStack(const MutantStack& copy) : std::stack<T, Container>(const MutantStack& copy) { //lo de std::stack<T, C> está bien?
+            *this : other;
+        }
+        ~MutantStack() {}
+
+        MutantStack& operator=(const MutantStack& other) {
+            if (this != &other) {
+                std::stack<T, Container>::operator=(other); //this-> necesario?
+            }
+            return *this;
+        }
     private:
-}
+};
 
 //investigar stack
 //investigar como añadir iteradores
