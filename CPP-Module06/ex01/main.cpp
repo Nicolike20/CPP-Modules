2f7776ddb51e94fd4bf239b5590884653aea943c
Nicolas maria Ortolan soto <nicolike3.0@gmail.com>
2024-03-04 16:54:25 +0100

    removed unused variable

--- CPP-Module06/ex01/main.cpp
+++ CPP-Module06/ex01/main.cpp
@@ -4,13 +4,12 @@
 
 int main() {
     Data *d = new Data;
-    Data *d2 = new Data;
 
     d->s = "Test";
     d->n = 42;
 
-    std::cout   << "data: s -> " << d->s
-                << "\n      n -> " << d->n << "\n\n";
+    std::cout   << "data:\ns -> " << d->s
+                << "\nn -> " << d->n << "\n\n";
 
     uintptr_t raw = Serializer::serialize(d);
 
@@ -18,8 +17,8 @@ int main() {
 
     Data *deser = Serializer::deserialize(raw);
 
-    std::cout   << "Deserialized: s -> " << deser->s
-                << "\n              n -> " << deser->n << "\n";
+    std::cout   << "Deserialized:\ns -> " << deser->s
+                << "\nn -> " << deser->n << "\n";
 
     delete d;
     return (0);
