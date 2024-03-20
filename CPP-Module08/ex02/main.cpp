9a827e7044b92a23a6f4ef007c59c33c277d76d7
Nicolas maria Ortolan soto <nicolike3.0@gmail.com>
2024-03-20 18:38:32 +0100

    made better tests in main

--- CPP-Module08/ex02/main.cpp
+++ CPP-Module08/ex02/main.cpp
@@ -6,53 +6,6 @@
 
 int main(void) {
 
-/*
-    MutantStack<int> mstack;                            //Subject test
-    mstack.push(5);
-    mstack.push(17);
-    std::cout << mstack.top() << std::endl;
-    mstack.pop();
-    std::cout << mstack.size() << std::endl;
-    mstack.push(3);
-    mstack.push(5);
-    mstack.push(737);
-    //[...]
-    mstack.push(0);
-    MutantStack<int>::iterator it = mstack.begin();
-    MutantStack<int>::iterator ite = mstack.end();
-    ++it;
-    --it;
-    while (it != ite)
-    {
-    std::cout << *it << std::endl;
-    ++it;
-    }
-    std::stack<int> s(mstack);
-
------------------------------------------------------
-
-    std::list<int> mstack;                               //Subject test (with lists)
-    mstack.push_back(5);
-    mstack.push_back(17);
-    std::cout << mstack.back() << std::endl;
-    mstack.pop_back();
-    std::cout << mstack.size() << std::endl;
-    mstack.push_back(3);
-    mstack.push_back(5);
-    mstack.push_back(737);
-    //[...]
-    mstack.push_back(0);
-    std::list<int>::iterator it = mstack.begin();
-    std::list<int>::iterator ite = mstack.end();
-    ++it;
-    --it;
-    while (it != ite)
-    {
-    std::cout << *it << std::endl;
-    ++it;
-    }
-    std::list<int> s(mstack);
-/*/
     MutantStack<int> iStack;                             //My test
     MutantStack<char> cStack;
     MutantStack<std::string> sStack;
@@ -74,43 +27,118 @@ int main(void) {
 
     auxStack.pop();
 
-    std::cout << "\033[4mCopy Stack after pop\033[24m\n";
+    std::cout << "\n\033[4mCopy Stack after pop\033[24m\n";
     std::cout << "Top ->  " << auxStack.top() << "\n";
     std::cout << "Size -> " << auxStack.size() << "\n";
 
     iStack.swap(auxStack);
 
-    std::cout << "\033[4mInt Stack after swap\033[24m\n";
+    std::cout << "\n\033[4mInt Stack after swap\033[24m\n";
     std::cout << "Top ->  " << iStack.top() << "\n";
     std::cout << "Size -> " << iStack.size() << "\n";
+
+    MutantStack<int>::iterator b1 = iStack.begin();
+    MutantStack<int>::iterator e1 = iStack.end();
+    std::cout << "Content:\n";
+    while (b1 != e1) {
+        std::cout << "·" << *b1 << "\n";
+        b1++;
+    }
 
     std::cout << "\033[4mCopy Stack after swap\033[24m\n";
     std::cout << "Top ->  " << auxStack.top() << "\n";
     std::cout << "Size -> " << auxStack.size() << "\n";
 
-
-
-
+    MutantStack<int>::iterator b2 = auxStack.begin();
+    MutantStack<int>::iterator e2 = auxStack.end();
+    std::cout << "Content:\n";
+    while (b2 != e2) {
+        std::cout << "·" << *b2 << "\n";
+        b2++;
+    }
+        //--------------------------------------//
     cStack.push('a');
     cStack.push('b');
     cStack.push('c');
     cStack.push('d');
     cStack.push('e');
-    std::cout << "\033[4mChar Stack\033[24m\n";
+    std::cout << "\n\033[4mChar Stack\033[24m\n";
     std::cout << "Top ->  " << cStack.top() << "\n";
     std::cout << "Size -> " << cStack.size() << "\n";
 
+    MutantStack<char>::iterator b3 = cStack.begin();
+    MutantStack<char>::iterator e3 = cStack.end();
+    std::cout << "Content:\n";
+    while (b3 != e3) {
+        std::cout << "·" << *b3 << "\n";
+        b3++;
+    }
+        //--------------------------------------//
     sStack.push("aaaaa");
     sStack.push("bbbbb");
     sStack.push("ccccc");
     sStack.push("ddddd");
     sStack.push("eeeee");
-    std::cout << "\033[4mChar Stack\033[24m\n";
+    std::cout << "\n\033[4mChar Stack\033[24m\n";
     std::cout << "Top ->  " << sStack.top() << "\n";
     std::cout << "Size -> " << sStack.size() << "\n";
 
-    //METER COSAS DE ITERADORES, SE ME HA IDO LA OLLA
-//*/
+    MutantStack<std::string>::iterator b4 = sStack.begin();
+    MutantStack<std::string>::iterator e4 = sStack.end();
+    std::cout << "Content:\n";
+    while (b4 != e4) {
+        std::cout << "·" << *b4 << "\n";
+        b4++;
+    }
+        //--------------------------------------//
+    std::cout << "\n\033[4mTests given in the subject\033[24m\n";
+    std::cout << "MutantStack test:\n";
+
+    MutantStack<int> mstack;                            //Subject test
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    //[...]
+    mstack.push(0);
+    MutantStack<int>::iterator it1 = mstack.begin();
+    MutantStack<int>::iterator ite1 = mstack.end();
+    ++it1;
+    --it1;
+    while (it1 != ite1)
+    {
+    std::cout << *it1 << std::endl;
+    ++it1;
+    }
+    std::stack<int> s1(mstack);
+
+    std::cout << "List test:\n";
+
+    std::list<int> lstack;                               //Subject test (with lists)
+    lstack.push_back(5);
+    lstack.push_back(17);
+    std::cout << lstack.back() << std::endl;
+    lstack.pop_back();
+    std::cout << lstack.size() << std::endl;
+    lstack.push_back(3);
+    lstack.push_back(5);
+    lstack.push_back(737);
+    //[...]
+    lstack.push_back(0);
+    std::list<int>::iterator it2 = lstack.begin();
+    std::list<int>::iterator ite2 = lstack.end();
+    ++it2;
+    --it2;
+    while (it2 != ite2)
+    {
+    std::cout << *it2 << std::endl;
+    ++it2;
+    }
+    std::list<int> s2(lstack);
     
     return (0);
 }
