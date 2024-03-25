b9366d3f83d6d9d4fa151527ad16a92af9816bcd
Nico <nicolike3.0@gmail.com>
2024-03-25 12:36:00 +0100

    not much

--- CPP-Module09/ex00/BitcoinExchange.hpp
+++ CPP-Module09/ex00/BitcoinExchange.hpp
@@ -1,9 +1,16 @@
 #ifndef BITCOINEXCHANGE_HPP_
 # define BITCOINEXCHANGE_HPP_
 
+# include <string>
+# include <map>
+
 class BitcoinExchange {
 	public:
-		
+		BitcoinExchange();
+		BitcoinExchange(const BitcoinExchange& copy);
+		~BitcoinExchange();
+
+		BitcoinExchange& operator=(const BitcoinExchange& other);
 	private:
 };
 
